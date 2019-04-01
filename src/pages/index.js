66de592bc6019027b86fabdf5b18496b18db190c
Kristian Alvarez Jörgensen <krijor@kth.se>
2019-04-01 14:39:05 +0200

    Fix linting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React from 'react';
 
-function IndexPage() {
-  return <h1>Hello world!</h1>
-}
+const IndexPage = () => (
+  <h1>Hello world!</h1>
+);
 
-export default IndexPage
+export default IndexPage;
