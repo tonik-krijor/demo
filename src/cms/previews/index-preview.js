6f139c48ae51a424d8ec0bc636e4c189bad893f1
Toni Karppi <karppitoni@gmail.com>
2019-05-01 21:32:12 +0200

    Remove conditional from index preview

--- src/cms/previews/index-preview.js
+++ src/cms/previews/index-preview.js
@@ -3,10 +3,5 @@ import { IndexPageContent } from '../../pages/index';
 
 export default function IndexPagePreview({ entry }) {
   const data = entry.getIn(['data']).toJS();
-
-  if (data) {
-    return <IndexPageContent image={data.image} title={data.title} />;
-  } else {
-    return <div>Loading...</div>;
-  }
+  return <IndexPageContent image={data.image} title={data.title} />;
 }
