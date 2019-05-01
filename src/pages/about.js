529378f8c6d50d75f2a40ac42af7b8679667f7fc
Toni Karppi <karppitoni@gmail.com>
2019-05-01 21:42:52 +0200

    Split about page to page and content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,38 +1,50 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
+import Metadata from '../components/metadata';
+import Layout from '../components/layout';
+import Header from '../components/header';
 
-export default ({
-  data: {
+export function AboutPageContent({ title, stuff }) {
+  return (
+    <Layout>
+      <div>
+        <h1>{title}</h1>
+        <div>
+          <ul>
+            {stuff.map(item => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default function AboutPage() {
+  const {
     file: {
       childMarkdownRemark: {
         frontmatter: { title, stuff }
       }
     }
-  }
-}) => {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <div>
-        <ul>
-          {stuff.map(item => (
-            <li>{item}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-export const query = graphql`
-  {
-    file(relativePath: { eq: "pages/about.md" }) {
-      childMarkdownRemark {
-        frontmatter {
-          title
-          stuff
+  } = useStaticQuery(graphql`
+    {
+      file(relativePath: { eq: "pages/about.md" }) {
+        childMarkdownRemark {
+          frontmatter {
+            title
+            stuff
+          }
         }
       }
     }
-  }
-`;
+  `);
+
+  return (
+    <Metadata>
+      <Header />
+      <AboutPageContent title={title} stuff={stuff} />
+    </Metadata>
+  );
+}
