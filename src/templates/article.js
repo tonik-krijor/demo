0ce469a2f742e6fd70f6830bea622a211fb7c337
Toni Karppi <karppitoni@gmail.com>
2019-05-03 23:02:23 +0200

    Split ArticleTemplate and its layout

--- src/templates/article.js
+++ src/templates/article.js
@@ -3,22 +3,24 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import Header from '../components/header';
 
-export default function ArticleTemplate({ data }) {
+const ArticleTemplateLayout = ({ date, html }) => (
+  <Layout>
+    <Header />
+    <div>
+      <span>
+        Published:
+        {date}
+      </span>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  </Layout>
+);
+
+const ArticleTemplate = ({ data }) => {
   const { markdownRemark } = data; // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark;
-  return (
-    <Layout>
-      <Header />
-      <div>
-        <span>
-          Published:
-          {frontmatter.date}
-        </span>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </div>
-    </Layout>
-  );
-}
+  return <ArticleTemplateLayout date={frontmatter.date} html={html} />;
+};
 
 export const query = graphql`
   query($id: String!) {
@@ -30,3 +32,6 @@ export const query = graphql`
     }
   }
 `;
+
+export default ArticleTemplate;
+export { ArticleTemplateLayout };
