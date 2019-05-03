679c8532f42d13eb0b763dcf3d42e635eb7e6fe1
Toni Karppi <karppitoni@gmail.com>
2019-05-03 19:14:32 +0200

    Change onCreateNode to add parent node field

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,13 @@
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  // Adds fields to remark nodes from parent file nodes.
+  // Creates a field for the parent file in the remark node.
   if (node.internal.type === 'MarkdownRemark') {
-    const { name, sourceInstanceName } = getNode(node.parent);
+    const parent = getNode(node.parent);
     const { createNodeField } = actions;
 
-    // contentDir is the name of the directory inside 'content/'.
     createNodeField({
       node,
-      name: 'sourceInstanceName',
-      value: sourceInstanceName
-    });
-
-    // fileName is the name of the markdown file (without the extension).
-    createNodeField({
-      node,
-      name: 'fileName',
-      value: name
+      name: 'file',
+      value: parent
     });
   }
 };
