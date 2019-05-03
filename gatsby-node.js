c30dc66c13779ae6ae80543c50f7e15c9461a08e
Toni Karppi <karppitoni@gmail.com>
2019-05-03 18:13:58 +0200

    Add fields from file nodes to remark nodes

---
+++ gatsby-node.js
@@ -0,0 +1,21 @@
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  // Adds fields to remark nodes from parent file nodes.
+  if (node.internal.type === 'MarkdownRemark') {
+    const { name, sourceInstanceName } = getNode(node.parent);
+    const { createNodeField } = actions;
+
+    // contentDir is the name of the directory inside 'content/'.
+    createNodeField({
+      node,
+      name: 'contentDir',
+      value: sourceInstanceName
+    });
+
+    // fileName is the name of the markdown file (without the extension).
+    createNodeField({
+      node,
+      name: 'fileName',
+      value: name
+    });
+  }
+};
