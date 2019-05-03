478a3e0784501b7873c47a7c8aa84843ba967dab
Toni Karppi <karppitoni@gmail.com>
2019-05-03 21:07:33 +0200

    Add programmatic generation of articles

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,37 @@
+exports.createPages = async ({ actions, graphql }) => {
+  const queryResult = await graphql(`
+    {
+      allMarkdownRemark(
+        filter: { fields: { file: { sourceInstanceName: { eq: "articles" } } } }
+        sort: { fields: [frontmatter___date], order: DESC }
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const { createPage } = actions;
+  const articleTemplate = `${__dirname}/src/templates/article.js`;
+  const edges = queryResult.data.allMarkdownRemark.edges;
+
+  edges.forEach(({ node }) => {
+    const { id, frontmatter } = node;
+    const { slug } = frontmatter;
+    createPage({
+      path: slug,
+      component: articleTemplate,
+      context: { id }
+    });
+  });
+};
+
 exports.onCreateNode = ({ node, actions, getNode }) => {
   // Creates a field for the parent file in the remark node.
   if (node.internal.type === 'MarkdownRemark') {
---
+++ src/templates/article.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
+import Header from '../components/header';
+
+export default function Article({ data }) {
+  const { markdownRemark } = data; // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark;
+  return (
+    <Layout>
+      <Header />
+      <div>
+        <span>Published: {frontmatter.date}</span>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
+`;
