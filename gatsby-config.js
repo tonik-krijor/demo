d7ef7c34f3fa2603d02cc21c6dc40848453540ab
Toni Karppi <karppitoni@gmail.com>
2019-05-03 11:26:37 +0200

    Split content filesystem sources

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,30 @@
 module.exports = {
   siteMetadata: {
     title: 'Demo Site',
-    description: 'A cool site',
+    description: 'A cool site'
   },
 
   plugins: [
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/content`,
-        name: 'content',
-      },
+        path: `${__dirname}/content/pages`,
+        name: 'pages'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/articles`,
+        name: 'articles'
+      }
     },
     'gatsby-transformer-remark',
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-  ],
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
+    }
+  ]
 };
